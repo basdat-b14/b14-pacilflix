56d8c85a26dd69745be0f97d3972ec45a08c3f37
Marchelina Anjani S <marselinaanjanisaputri23@gmail.com>
2024-04-29 21:30:55 +0700

    ni

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from b14_pacilflix import settings
 from . import views
+from django.conf.urls.static import static
 
 app_name = 'main'
 
@@ -7,3 +9,6 @@ app_name = 'main'
 urlpatterns = [
     path('', views.show_main, name = 'show_main'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
