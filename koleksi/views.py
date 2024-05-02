96a472692635ac235277f41b258307e8a48c29dd
M.Syahrul Khaliq <syahrulkhaliq58@gmail.com>
2024-05-02 18:11:07 +0700

    daftar unduhan dan favorit

--- koleksi/views.py
+++ koleksi/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def contributor_list_view(request):
     return render(request, 'Daftar_Kontributor.html')
+
+def daftar_unduhan_view(request): 
+    return render(request, 'Daftar_Unduhan.html')
+
+def daftar_favorit_view(request): 
+    return render(request, 'Daftar_Favorit.html')
