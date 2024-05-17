ef9d6f9a20ee618837c3370de4b7175c826d82ea
bulan <bulan.athaillah@gmail.com>
2024-05-17 18:17:08 +0700

    all done except trigger and handling paket

--- media_tayangan/urls.py
+++ media_tayangan/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('cari-tayangan', cari_tayangan, name='cari_tayangan'),
     path('series/<str:judul>/', series_view, name='series_view'),
     path('film/<str:judul>/', film_view, name='film_view'),
-    path('episode', episode_view, name='episode_view'),
+    path('episode/<str:judul>/', episode_view, name='episode_view'),
+    path('save-progress-series', save_progress_series, name='save_progress_series'),
+    path('save-progress-film', save_progress_film, name='save_progress_film'),
+    path('ulasan', ulasan, name='ulasan')
 ]
