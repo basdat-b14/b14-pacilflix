6cb04be3f9e4ef3ecf554a832e07fdb26c9c17bc
Amertaaa <syahrulkhaliq58@gmail.com>
2024-05-19 21:53:15 +0700

    fix navbar

--- koleksi/urls.py
+++ koleksi/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('daftar_favorit/', views.daftar_favorit_view, name='daftar_favorit'), # Map the view to the root URL of the app
 
     # path('delete_tayangan/', views.delete_tayangan, name='delete_tayangan'),
-    path('delete_unduhan/<str:nama_tayangan>/<str:username>/<uuid:id_tayangan>/', views.delete_unduhan, name='delete_unduhan')
+    path('delete_unduhan/<str:nama_tayangan>/<str:username>/<uuid:id_tayangan>/', views.delete_unduhan, name='delete_unduhan'),
 
     # path('contributors/', views.contributors_list, name='contributor_list'),
     path('daftar_kontributor/', views.contributors_list, name='daftar_kontributor_page'),
